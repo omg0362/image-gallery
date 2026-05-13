34cbcccbf5d09ebfd7453628e75a16ac3a29c77a
Lee Ye-jun <omg0362@gmail.com>
2026-05-13 20:41:28 +0900

    part 1 완성

---
+++ app/hello/bye/page.tsx
@@ -0,0 +1,7 @@
+export default function ByePage() {
+    return (
+        <div>
+            <h1>가지 마세요</h1>
+        </div>
+    );
+}
---
+++ app/hello/page.tsx
@@ -0,0 +1,7 @@
+export default function HelloPage() {
+    return (
+        <div>
+            <h1>내용이 너무 유익해요</h1>
+        </div>
+    );
+}
